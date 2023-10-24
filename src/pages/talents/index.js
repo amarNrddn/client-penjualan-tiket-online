cc2156d4a0d3d6e3e5ba10ad51083bc340529f2e
amarNrddn <amarnrddn02@gmail.com>
2023-10-24 23:04:28 +0700

    fetch talent

--- src/pages/talents/index.js
+++ src/pages/talents/index.js
@@ -1,27 +1,61 @@
-import React, { useEffect } from 'react'
-import {  useSelector } from 'react-redux/es/hooks/useSelector'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux/es/hooks/useSelector'
 import { useDispatch } from 'react-redux'
 import { fetchTalents } from '../../redux/talents/action'
-import SearchInput from '../../components/Search'
-import { Container, Table } from 'react-bootstrap'
+import SearchInput from '../../components/SSearch'
+import { Container} from 'react-bootstrap'
+import  Table from '../../components/TableWithAction'
 import SBreadcrumbs from '../../components/Breadcrumbs'
+import { accessTalents } from '../../consts/access'
 
 const TalentsPage = () => {
   const dispatch = useDispatch()
   const talents = useSelector((state) => state.talents)
+  const [access, setAccess] = useState({
+    tambah: false,
+    hapus: false,
+    edit: false
+  })
+
+  const checkAccess = () => {
+    let { role } = localStorage.getItem('auth')
+      ? JSON.parse(localStorage.getItem('auth'))
+      : {}
+    const access = { tambah: false, hapus: false, edit: false }
+    Object.keys(accessTalents).forEach(function (key, index) {
+      if (accessTalents[key].indexOf(role) >= 0) {
+        access[key] = true
+      }
+    })
+    setAccess(access)
+  }
+
+  useEffect(() => {
+    checkAccess()
+  }, [])
 
   useEffect(() => {
     dispatch(fetchTalents())
   }, [dispatch])
-  return (
-      <Container>
-        <SBreadcrumbs textSecound={'Talents'}/>
-        <SearchInput />
 
-        <Table 
-          status={talents.status}
-        />
-      </Container>
+  const handleDelet = (id) => {
+
+  }
+  return (
+    <Container>
+      <SBreadcrumbs textSecound={'Talents'} />
+      <SearchInput />
+
+      <Table
+        status={talents.status}
+        thead={['Nama', 'Role', 'Avatar', 'Aksi']}
+        data={talents.data}
+        tbody = {['name', 'role', 'avatar']}
+        edit={access.edit ? `/talents/edit` : null}
+        deleteAction={access.hapus ? (id) => handleDelet(id) : null}
+        withoutPagination
+      />
+    </Container>
   )
 }
 
